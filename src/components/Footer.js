c8434bf52ad4458db70a3ae09d9f54acb1daf13c
9710willy <myungjoon.lee@vanderbilt.edu>
2020-06-08 11:33:46 -0500

    Added Footer, Changed Graph Color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <p>Footer</p>;
+  return (
+    <footer className="footer">
+      <a href="mailto:reactcdv@gmail.com" class="footer_link">
+        reactcdv@gmail.com
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
