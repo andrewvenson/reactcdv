16f0867bf97f588efce1fa9eff805a85eee3d029
Andrew Venson <vensoneandrew@gmail.com>
2020-06-14 22:22:38 -0500

    added functionality to return polygon data from countries.geo.json

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Polygon,
+} from "react-google-maps";
 
 function Map() {
   const navTab = {
@@ -53,13 +58,86 @@ function Map() {
     }
   }
 
+  const data = require("../geojsons/countries.geo.json");
+
+  let coordsObjArr = {};
+
+  for (let country in data["features"]) {
+    if (data["features"][country].geometry.type === "MultiPolygon") {
+      coordsObjArr[data["features"][country].properties.name] = {};
+      data["features"][country].geometry.coordinates.forEach(
+        (coords, index) => {
+          coordsObjArr[data["features"][country].properties.name][
+            `${index}${data["features"][country].properties.name}`
+          ] = [];
+          coords[0].forEach((coo) => {
+            coordsObjArr[data["features"][country].properties.name][
+              `${index}${data["features"][country].properties.name}`
+            ].push({
+              lat: coo[1],
+              lng: coo[0],
+            });
+          });
+        }
+      );
+    } else {
+      coordsObjArr[data["features"][country].properties.name] = [];
+
+      data["features"][country].geometry.coordinates[0].forEach((coords) => {
+        coordsObjArr[data["features"][country].properties.name].push({
+          lng: coords[0],
+          lat: coords[1],
+        });
+      });
+    }
+  }
+
   // creates map component
   const CdvMapComponent = withScriptjs(
     withGoogleMap((props) => (
       <GoogleMap
         defaultZoom={mapview.defaultZoom}
         defaultCenter={mapview.defaultCenter}
-      ></GoogleMap>
+      >
+        {/* Returns polygon data layer */}
+        {Object.keys(coordsObjArr).map((coords, index) => {
+          // if Data type is polygon return simple array
+          if (Array.isArray(coordsObjArr[coords])) {
+            // return array of coordinates in polygon
+            return (
+              <Polygon
+                path={coordsObjArr[coords]}
+                key={index}
+                options={{
+                  fillColor: "#000",
+                  fillOpacity: 0.4,
+                  strokeColor: "#000",
+                  strokeOpacity: 1,
+                  strokeWeight: 1,
+                }}
+              />
+            );
+          } else {
+            // if data type is multipolygon map through objects within object
+            return Object.keys(coordsObjArr[coords]).map((coo, ind) => {
+              // return array of coordinates
+              return (
+                <Polygon
+                  path={coordsObjArr[coords][coo]}
+                  key={ind}
+                  options={{
+                    fillColor: "#eb4034",
+                    fillOpacity: 0.4,
+                    strokeColor: "#000",
+                    strokeOpacity: 1,
+                    strokeWeight: 1,
+                  }}
+                />
+              );
+            });
+          }
+        })}
+      </GoogleMap>
     ))
   );
 
